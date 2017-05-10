6c7c2f5233e4d708d225a456ae1a14bfdcc8c95f
Silas <silascofc@gmail.com>
2017-05-09 23:50:35 -0300

    Refatorando settings para locations - Adicionando mapa em ponto fixo

--- src/pages/locations/locations.ts
+++ src/pages/locations/locations.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var google;
+
 @Component({
-  selector: 'settings',
-  templateUrl: 'settings.html'
+  selector: 'page-locations',
+  templateUrl: 'locations.html'
 })
 export class LocationsPage {
+  @ViewChild('map') mapElement;
+  map: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
+  ionViewDidLoad() {
+    this.initMap();
   }
 
+  initMap() {
+    let latLng = new google.maps.LatLng(-10.2001163,-48.3204604);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 14,
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
 }
