e0681f6862fa476ae509ac1b990d136cc97fd6c7
Silas <silascofc@gmail.com>
2017-05-17 00:37:34 -0300

    Sofrendo sem evoluir

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+import { LocationAccuracy } from '@ionic-native/location-accuracy';
 import {
   GoogleMap,
   GoogleMaps,
@@ -16,8 +18,7 @@ import {
 })
 export class MapPage {
 
-  constructor(public navCtrl: NavController, public googleMaps: GoogleMaps) {
-
+  constructor(public navCtrl: NavController, public googleMaps: GoogleMaps, public geolocation: Geolocation, public locationAccuracy: LocationAccuracy, public alertCtrl: AlertController) {
   }
 
   ngAfterViewInit() {
@@ -27,23 +28,38 @@ export class MapPage {
   loadMap() {
     let element = document.getElementById('map');
     let map: GoogleMap = this.googleMaps.create(element, {});
-    let latlng = new LatLng(-10.1779967, -48.3340244);
+    let latlng: LatLng = new LatLng(-10.2001416, -48.3200967);
+    let options = {
+      enableHighAccuracy: true
+    };
+
+    // this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(() => {
+    //   this.geolocation.getCurrentPosition(options).then((position: Geoposition) => {
+    //     latlng = new LatLng(position.coords.latitude, position.coords.longitude);
+    //   }).catch((err) => {
+    //     alert(err);
+    //   });
+    // }, (error) => {
+    //   alert(error);
+    // });
 
     map.one(GoogleMapsEvent.MAP_READY).then(() => {
       let position: CameraPosition = {
         target: latlng,
-        zoom: 13,
+        zoom: 14,
         tilt: 30
       }
+
       map.moveCamera(position);
       let markeroptions: MarkerOptions = {
         position: latlng,
-        title: 'Palácio Araguaia'
+        title: 'Minha localização'
       };
 
       map.addMarker(markeroptions).then((marker: Marker) => {
         marker.showInfoWindow();
       });
+
     });
   }
 }
